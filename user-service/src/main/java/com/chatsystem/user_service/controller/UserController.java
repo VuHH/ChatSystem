36db8706e7b2deff343a757dfa9eae3640f89f31
VuHH <hohoanvu1993@gmail.com>
2024-11-29 14:03:16 +0700

    - Update Filter

--- user-service/src/main/java/com/chatsystem/user_service/controller/UserController.java
+++ user-service/src/main/java/com/chatsystem/user_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.chatsystem.user_service.controller;
 
 import com.chatsystem.user_service.entity.User;
 import com.chatsystem.user_service.service.KafkaProducerService;
+import com.chatsystem.user_service.service.UserService;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,21 +10,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
   private final KafkaProducerService kafkaProducerService;
+  private final UserService userService;
 
-  public UserController(KafkaProducerService kafkaProducerService) {
+  public UserController(KafkaProducerService kafkaProducerService, UserService userService) {
     this.kafkaProducerService = kafkaProducerService;
+    this.userService = userService;
   }
 
-  //  @PostMapping("/filters")
-  //  public String filter(@RequestBody String username) {
-  //    //return jwtUtil.generateToken(username);
-  //  }
+  @PostMapping("/filters")
+  public User filter(@RequestBody String phoneNumber) {
+    return userService.findUser(phoneNumber);
+  }
 
   @PostMapping
   public String addUser(@RequestBody List<User> users) {
     for (User u : users) {
       kafkaProducerService.sendUserEvent("user-topic", u);
     }
-    return "Added users";
+    return "Added will add users";
   }
 }
